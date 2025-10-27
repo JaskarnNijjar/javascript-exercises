6a2853829aa8e5e53a74f3780e5ae7eff2df88c5
Jaskarn Nijjar <jaskarn.nijjar75@gmail.com>
2025-10-26 20:58:50 -0700

    11_tempConversion complete

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  return Math.round(((tempF - 32) * 5/9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  return Math.round(((tempC * 9/5) + 32)* 10) / 10;
 };
 
 // Do not edit below this line
@@ -9,3 +11,22 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+
+// # Exercise 11 - tempConversion
+
+// Write two functions that convert temperatures from Fahrenheit to Celsius, and vice versa:
+// ```
+// convertToCelsius(32) // fahrenheit to celsius, should return 0
+
+// convertToFahrenheit(0) // celsius to fahrenheit, should return 32
+// ```
+
+// Because we are human, we want the result temperature to be rounded to one decimal place: i.e., `convertToCelsius(100)` should return `37.8` and not `37.77777777777778`.
+
+// This exercise asks you to create more than one function so the `module.exports` section of the main javascript file looks a little different this time. Nothing to worry about, we're just packaging both functions into a single object to be exported.
+
+// ## Hints
+// - You can find the relevant formulae on [Wikipedia](https://en.wikipedia.org/wiki/Conversion_of_units_of_temperature).
+
+// - Try to find by yourself on the Internet how to round a number to 1 decimal place in JavaScript. If you struggle, have a look [here](https://stackoverflow.com/q/7342957/5433628).
